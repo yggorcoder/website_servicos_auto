13a20a4630e353061534ce8ad9f63df7cdce8a63
yggorcoder <yggor.ramos@gmail.com>
2026-06-20 08:22:50 -0300

    Adiciona Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -20,6 +21,7 @@ export default function App() {
       </main>
       <Footer />
       <WhatsAppButton />
+      <Analytics />
     </>
   );
 }
